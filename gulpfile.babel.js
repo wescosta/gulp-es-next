5829643ac89b8d63546072fa25ef4e5d804a9101
Wesley Anemam <wesleylira@gmail.com>
2015-10-10 15:28:11 +0100

    Fixing del import in ES6 syntax

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import {src, dest, task, $} from 'gulp-es-next'
-import {del} from 'del'
+import del from 'del'
 
 task('transpile', ['clean'], () => {
 	src('node_modules/gulp-es-next.js')
